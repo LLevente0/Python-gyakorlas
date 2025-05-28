91a10ef3577bc6e4c9555b09f466ea7f6389152f
LLevente <ladanyi.levente@diak.szbi-pg.hu>
2025-05-28 13:04:14 +0200

    Gyakorlas

---
+++ gyakorlo.py
@@ -0,0 +1,90 @@
+"""1. Egyszerű számbekérő
+Kérj be egy számot a felhasználótól, és döntsd el, hogy páros vagy páratlan. Írd ki az eredményt!
+Pl. 5
+print:
+Az 5-ös szám páratlan szám.
+"""
+
+def szambekero():
+    szam = int(input("Kérlek adj meg egy számot: "))
+
+    if szam % 2 == 0:
+        print(f"Ez a szám {szam} és páros.")
+    else:
+        print(f"Ez a szám {szam} és páratlan.")
+
+
+
+"""
+2. Összegszámítás
+Kérj be egy egész számot, és számítsd ki az 1-től a megadott számig terjedő egész számok összegét.
+pl. 7
+print:
+1
+2
+3
+4
+5
+6
+7
+"""
+
+def osszegszamitas():
+    szam = int(input("Adj meg egy egész számot: "))
+
+    osszeg = 0
+
+    for i in range(1, szam + 1):
+        print(i)
+        osszeg += i
+
+    print("Összeg:", osszeg)
+
+
+
+
+
+
+"""
+3. Számok listázása és összegzése
+a) Írj egy programot, amely bekér n számot a felhasználótól, majd egy while ciklussal megkérdezi a felhasználót, 
+hogy szeretne-e újabb számot megadni. Addig folytassa a program a számok bekérését, amíg a felhasználó igennel válaszol.
+A program végén jelenítse meg a bekért számok összegét.
+Pl. 5
+    igen
+    6
+    nem
+
+print: A számok összege: 11
+"""
+
+def listazas():
+    szamok = []
+    szam = int(input("Kérlek adj meg egy számot: "))
+    szamok.append(szam)
+
+    while True:
+        valasz = input("Szeretnél új számot megadni? ").strip().lower()
+        if valasz == "igen":
+            szam = int(input("Kérlek adj meg egy számot: "))
+            szamok.append(szam)
+        elif valasz == "nem":
+            break
+
+
+
+
+listazas()
+
+
+
+
+"""
+b) jelenítsd meg a bekért számokat (lista használata)
+Pl. 5
+    igen
+    6
+    nem
+
+print: A számok: [5, 6]
+"""
---
+++ gyakorlo2.py
@@ -0,0 +1,47 @@
+"""
+4. Két szám közötti számok
+Kérj be két számot a felhasználótól (a és b). Írasd ki az összes számot a és b között.
+Pl. 5 és 10
+
+print: 5, 6, 7, 8, 9, 10
+"""
+
+
+
+
+
+"""
+b) Ha a nagyobb, mint b, akkor csökkenő sorrendben írasd ki őket.
+Pl. 10 és 5
+
+print: 10, 9, 8, 7, 6, 5
+"""
+
+
+
+
+
+
+"""
+5. Egyszerű jelszókérés
+Készíts egy programot, amely egy előre meghatározott jelszót vár el a felhasználótól.
+A program addig kérdez, amíg a helyes jelszót meg nem adják. Ha eltalálja a jelszót, jelenjen meg egy üzenet, hogy „Sikeres belépés”.
+Pl. jelszo = titkos
+"""
+
+
+
+
+
+
+"""
+6. Szorzótábla
+Írasd ki egy adott szám szorzótábláját 1-től 10-ig. Például, ha a felhasználó 5-öt ad meg, akkor az eredmény legyen:
+Pl. 5
+
+print:
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50
+"""
